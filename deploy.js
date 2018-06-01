19b99337ac63fc1558ae6df948887dc1a33282c6
tjang0806 <janghj12000@gmail.com>
2018-06-01 10:46:59 -0700

    use private key instead of mnemonic

--- deploy.js
+++ deploy.js
@@ -1,15 +1,29 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
+
+//--------------------private key case--------------------------------
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+
 const Web3 = require('web3');
 // created by compile script
 const { interface, bytecode } = require('./compile');
 
 // specifying the provider with two parameter
-// 1.nmonic (in my case, I used ganache )
+// 1.private key
 // 2.url where to deploy (http://localhost:8545/ is the ganache's url)
 const provider = new HDWalletProvider(
-  'stumble region click broccoli pupil one tag boost whip spin over live',
-  'http://localhost:8545/'
+  'fabd0ab7fac67f1c3aa6e2a3a78eec384ca7ab6bae9e3a4467fa262757509d5c',
+  'http://localhost:6545/'
 );
+------------------------------------------------------------------------
+//------------------------ mnemonic case -------------------------------
+//using mnemonic
+//const HDWalletProvider = require('truffle-hdwallet-provider');
+/*const provider = new HDWalletProvider(
+  //'stumble region click broccoli pupil one tag boost whip spin over live',
+  'http://localhost:8545/'
+);*/
+//const web3 = new Web3(provider);
+//-----------------------------------------------------------------------
+
 const web3 = new Web3(provider);
 
 // deploy script
@@ -17,8 +31,11 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
+  //account's ether amount
+  //const etherAmount =  await web3.fromWei(eth.getBalance(eth.coinbase), "ether");
+
   // display account
-  console.log('attempting to deploy from account', accounts[0]);
+  console.log('attempting to deploy from account ' + accounts[0] );
 
   // in data, I add '0x' to save gas(found from stackoverflow)
       //it wiill assume the bytecode in hexadecimal
